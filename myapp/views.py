2ad7e0f5f810de2e862ec7b3b9a8b63d33ab50ae
Sayed Nahid <nahid15-3849@diu.edu.bd>
2024-08-11 19:40:34 +0600

    Added forms.py file for handling Post forms

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from .forms import PostForm
 # Create your views here.
+#views for rendering page
 def index(request):
     return render(request, 'index.html')
 def book(request):
@@ -11,3 +13,6 @@ def menu(request):
     return render(request, 'menu.html')
 def FrenchFries(request):
     return render(request, 'FrenchFries.html')
+
+
+#CRUD Operations for Posting food Reviews
