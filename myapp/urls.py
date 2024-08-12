bfdb81b3628e59948cb6114f5518072acda55946
Sayed Nahid <nahid15-3849@diu.edu.bd>
2024-08-12 23:42:25 +0600

    Create post functionality is created

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,10 +3,14 @@ from django.urls import path, include
 from myapp import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    #path('', views.index, name="index"),
     path('index', views.index, name="index"),
     path('menu', views.menu, name="menu"),
     path('about', views.about, name="about"),
     path('book', views.book, name="book"),
     path('FrenchFries', views.FrenchFries, name="FrenchFries"),
+    path('postlist', views.post_list, name="post_list"),
+    path('createpost', views.post_create, name="post_create"),
+    path('<int:post_id>/editpost', views.post_edit, name='post_edit'),
+    path('<int:post_id>/deletepost', views.post_delete, name='post_delete'),
 ]
