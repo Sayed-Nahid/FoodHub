054ed630f1c8db76ba493f501dfb357fbcbf7e46
Sayed Nahid <nahid15-3849@diu.edu.bd>
2024-08-15 19:28:00 +0600

    fixed some error

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from myapp import views
 
 urlpatterns = [
-    #path('', views.index, name="index"),
+    path('', views.index, name="index"),
     path('index', views.index, name="index"),
     path('menu', views.menu, name="menu"),
     path('about', views.about, name="about"),
